29d157c5f454ce45ee2b08d8e5a5746c3c85fac0
colzak <joel.lauret@gmail.com>
2014-03-17 10:54:09 +0100

    add contact

--- src/Colzak/HomeBundle/Controller/HomeController.php
+++ src/Colzak/HomeBundle/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace Colzak\HomeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Colzak\HomeBundle\Document\Message;
 use Colzak\HomeBundle\Form\Type\ContactFormType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class HomeController extends Controller
 {
@@ -17,6 +18,24 @@ class HomeController extends Controller
         $dm = $this->get('doctrine_mongodb')->getManager();
         $message = new Message();
         $form = $this->get('form.factory')->create(new ContactFormType(), $message);
+
+        $request = $this->getRequest();
+
+        if($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $message = $form->getData();
+                $dm->persist($message);
+                $dm->flush();
+
+                $this->get('session')->getFlashBag()->add(
+                    'success',
+                    'Votre message à bien été envoyé ! Vous recevrez une réponse dans les plus bref délais.'
+                );
+                return new RedirectResponse($this->container->get('router')->generate('colzak_home_contact'));
+            }
+        }
 
         return $this->render('ColzakHomeBundle:Home:contact.html.twig', array('form' => $form->createView()));
     }
